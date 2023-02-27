0623808b646799092d32f9f3b08f3733778adfc4
RudyPastel <rudy.pastel@gmail.com>
2023-02-27 17:16:11 +0100

    A built up the options

--- R/visualiseIncomeDataModule.R
+++ R/visualiseIncomeDataModule.R
@@ -39,16 +39,49 @@ visualiseIncomeDataPlotUi  = function(id){
   fluidRow(
     column(
       width = 12,
-      uiOutput(outputId = NS(id,'plot'))
+      plotOutput(outputId = NS(id,'plot'))
     )
   )
 }
 
 #' @rdname visualiseIncomeDataSelectionUi
-visualiseIncomeDataServer  = function(id){
+visualiseIncomeDataPlotServer  = function(id){
   moduleServer(
     id = id,
     module = function(input, output, session){
 
+      # Get the income data set
+      incomeDataSet = reactive({getIncomeDataSet()})
+
+      # Build the option menu
+      output$option = renderUI({
+        if (input$selection %in% c('medianIncome', 'meanIncome')){
+          ui = radioButtons(inputId = session$ns('includeMarginOfError'),
+                            label = 'Include Margin of error',
+                            choices = list('Yes' = TRUE, 'No' = FALSE),
+                            inline = TRUE,
+                            selected = FALSE)
+        } else if (input$selection %in% c('incomeDistribution')) {
+          ui = sliderInput(inputId = session$ns('selectedYear'),
+                           label = "Year",
+                           min = min(incomeDataSet()[['year']]),
+                           max = max(incomeDataSet()[['year']]),
+                           value = median(unique(incomeDataSet()[['year']]), na.rm = TRUE),
+                           step = 1,
+                           sep = '',
+                           animate = animationOptions(interval = 100)
+                           )
+        }else {
+          ui = NULL
+        }
+        return(ui)
+      })
+
+      # Build the plot
+      output$plot = renderPlot({
+        plot(cars)
+      })
     })
+
+
 }
